6050a5b35aa6f8d0142a412e484180009a308438
yxqsnz <yxqsnz@gmail.com>
2022-12-18 21:44:07 -0300

    Carbon the fox

--- User/Shell/Fish/conf.d/aliases.fish
+++ User/Shell/Fish/conf.d/aliases.fish
@@ -1,4 +1,6 @@
 alias ls='exa --icons'
 alias cat='bat -p --theme=ansi'
-alias gp='git push'
-alias gcm='git commit -m'
+
+abbr -a gp    'git push'
+abbr -a gcm   'git commit -m'
+abbr -a clone 'git clone'
