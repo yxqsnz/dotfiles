5cf5a07afb8c3d81ac475195e4db409f9eb65dab
yxqsnz <yxqsnz@gmail.com>
2023-03-19 13:00:41 -0300

    feat: better multimedia

--- conf/shell/fish/conf.d/aliases.fish
+++ conf/shell/fish/conf.d/aliases.fish
@@ -4,4 +4,5 @@ alias nv=nvim
 alias pls=sudo
 alias ls=exa
 alias py=python3
+alias dl-mp3="yt-dlp -f 'ba' -x --audio-format mp3 --embed-metadata --write-playlist-metafiles"
 alias oc-enter='eval $(opam env)'
